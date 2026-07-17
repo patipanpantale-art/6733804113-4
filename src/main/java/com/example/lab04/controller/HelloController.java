1cd8191304397c0d5548ae385a7a2c242cbbe74c
patipanpantale-art <patipanpantale@gmail.com>
2026-07-17 16:12:52 +0700

    feat: update student info - ID 673380411-3 sec 4

--- src/main/java/com/example/lab04/controller/HelloController.java
+++ src/main/java/com/example/lab04/controller/HelloController.java
@@ -28,7 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     // ── ข้อมูลนักศึกษา ── แก้ไขตรงนี้ ──────────────────────────────────────
-    private static final String NAME    = "นายปฏิภาณ ปานทะเล";     // TODO: ใส่ชื่อจริง
+    private static final String NAME    = "นายปฏิภาณ  ปานทะเล";     // TODO: ใส่ชื่อจริง
     private static final String ID      = "673380411-3";               // TODO: ใส่รหัสนักศึกษา
     private static final String SECTION = "4";                  // TODO: ใส่เลข Section
     // ─────────────────────────────────────────────────────────────────────────
